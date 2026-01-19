4748c52d581d67883330baf1234039b4449f4fa2
Lorenzo Vivenzio <lorenzovivenzio988@hotmail.com>
2026-01-19 17:53:28 +0100

    feat: rotta Show

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -1,19 +1,32 @@
 import connection from "../database/db.js"
 
-function index(req, res, next){
+function index(req, res, next) {
 
     const query = "SELECT * FROM `movies`";
 
-    connection.query(query, (err, result)=>{
+    connection.query(query, (err, result) => {
         if (err) return next(err)
         return res.json({
-            results : result,
+            results: result,
         })
     })
 }
 
-function show(req, res){
-console.log("show")
+function show(req, res) {
+
+    const id = req.params.id
+
+    const query = 'SELECT * FROM movies WHERE id = ?';
+    connection.query(query, [id], (err, results) => {
+        if (err) 
+            return res.status(500)
+        .json({ error: 'Database query failed' });
+        if (results.length === 0) 
+            return res.status(404)
+        .json({ error: 'Movie not found' });
+
+        res.json(results[0]);
+    });
 }
 
 export default {
